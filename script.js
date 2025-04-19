76a146f8e6da408f2f29162360859e29905a0f6a
Vinay0011 <132700694+Vinay0011@users.noreply.github.com>
2025-04-19 15:50:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-window.onload = function() {
-    Tabletop.init({
-      key: 'https://docs.google.com/spreadsheets/d/1tgy-c34BYJ3tfpqbcnBOOXqrpyq36v72PR21MacyD1Y/edit?usp=sharing',
-      simpleSheet: true,
-      callback: function(data) {
-        displayProducts(data);
-      }
-    });
-  };
-  
-  function displayProducts(products) {
-    const container = document.getElementById('productContainer');
-    container.innerHTML = '';
-  
-    products.forEach(product => {
-      const card = document.createElement('div');
-      card.className = 'product-card';
-      card.innerHTML = `
-        <img src="${product['Image Link 1']}" alt="${product['Product Name']}">
-        <div class="content">
-          <h2>${product['Product Name']}</h2>
-          <p class="description">${product['Description']}</p>
-          <p class="price">${product['Price']}</p>
-          <a class="order-btn" href="${product['Order Link']}" target="_blank">Order Now</a>
-        </div>
-      `;
-      container.appendChild(card);
-    });
-  }
+window.onload = function () {
+  Tabletop.init({
+    key: 'https://docs.google.com/spreadsheets/d/1tgy-c34BYJ3tfpqbcnBOOXqrpyq36v72PR21MacyD1Y/edit?usp=sharing',
+    callback: showData,
+    simpleSheet: true
+  });
+};
+
+function showData(data) {
+  console.log(data); // Data check karne ke liye
+  const container = document.getElementById("product-container");
+  container.innerHTML = "";
+
+  data.forEach(product => {
+    const card = document.createElement("div");
+    card.className = "product-card";
+
+    card.innerHTML = `
+      <div class="product-image">
+        <img src="${product.Image1}" alt="${product.Name}">
+      </div>
+      <div class="product-info">
+        <h2>${product.Name}</h2>
+        <p><strong>Price:</strong> ₹${product.Price}</p>
+        <p>${product.Description}</p>
+        <a class="order-button" href="https://wa.me/919285539561?text=I'm%20interested%20in%20${encodeURIComponent(product.Name)}" target="_blank">Order Now</a>
+      </div>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
   
